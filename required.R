b6e1cf21909d16548a0e2c6ffee2dfb31eee78fb
Cathy Yan <cayan@marrahost02.bcgsc.ca>
2024-08-19 14:27:32 -0700

    Created page for immune analyses

---
+++ required.R
@@ -0,0 +1,16 @@
+# data processing
+library(tidyverse)
+library(data.table)
+library(Matrix)
+library(Seurat)
+library(Signac)
+
+# plotting
+library(ggpubr)
+library(ggsci)
+library(RColorBrewer)
+library(ggrepel)
+library(cowplot)
+
+path <- "/projects/marralab/cayan_prj/PrecisionMed/Objects/"
+files <- list.files(path)
